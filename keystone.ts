b085be29622c6d04f94fcfd487540d5b616393ee
Irsan mansyur <irsan00mansyur@gmail.com.com>
2022-09-13 15:30:18 +0700

    Update schema

--- keystone.ts
+++ keystone.ts
@@ -5,43 +5,43 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const {
-	S3_BUCKET_NAME: bucketName = "keystone-test",
-	S3_REGION: region = "ap-southeast-2",
-	S3_ACCESS_KEY_ID: accessKeyId = "keystone",
-	S3_SECRET_ACCESS_KEY: secretAccessKey = "keystone",
-	ASSET_BASE_URL: baseUrl = "http://localhost:3000",
+  S3_BUCKET_NAME: bucketName = "keystone-test",
+  S3_REGION: region = "ap-southeast-2",
+  S3_ACCESS_KEY_ID: accessKeyId = "keystone",
+  S3_SECRET_ACCESS_KEY: secretAccessKey = "keystone",
+  ASSET_BASE_URL: baseUrl = "http://localhost:3000",
 } = process.env;
 
 export default withAuth(
-	config({
-		db: {
-			provider: "sqlite",
-			url: "file:./keystone.db",
-		},
-		ui: {
-			isAccessAllowed: (context) => !!context.session?.data,
-		},
-		session,
-		lists,
-		storage: {
-			my_local_images: {
-				kind: "local",
-				type: "image",
-				generateUrl: (path) => `${baseUrl}/images${path}`,
-				serverRoute: {
-					path: "/images",
-				},
-				storagePath: "public/images",
-			},
-			my_s3_files: {
-				kind: "s3",
-				type: "file",
-				bucketName,
-				region,
-				accessKeyId,
-				secretAccessKey,
-				signed: { expiry: 5000 },
-			},
-		},
-	})
+  config({
+    db: {
+      provider: "sqlite",
+      url: "file:./keystone.db",
+    },
+    ui: {
+      isAccessAllowed: (context) => !!context.session?.data,
+    },
+    session,
+    lists,
+    storage: {
+      my_local_images: {
+        kind: "local",
+        type: "image",
+        generateUrl: (path) => `${baseUrl}/images${path}`,
+        serverRoute: {
+          path: "/images",
+        },
+        storagePath: "public/images",
+      },
+      my_s3_files: {
+        kind: "s3",
+        type: "file",
+        bucketName,
+        region,
+        accessKeyId,
+        secretAccessKey,
+        signed: { expiry: 5000 },
+      },
+    },
+  })
 );
--- schema.ts
+++ schema.ts
@@ -1,132 +1,116 @@
 import { list } from "@keystone-6/core";
-import {
-	text,
-	relationship,
-	password,
-	timestamp,
-	select,
-	multiselect,
-	image,
-	file,
-} from "@keystone-6/core/fields";
+import { text, relationship, password, timestamp, select, multiselect, image, file } from "@keystone-6/core/fields";
 
 import { document } from "@keystone-6/fields-document";
 import { Lists } from ".keystone/types";
+import { CareerSchema } from "./schema/career.schema";
+import { ProductSchema } from "./schema/product.schema";
 
 // User Form
 export const lists: Lists = {
-	User: list({
-		fields: {
-			name: text({ validation: { isRequired: true } }),
-			email: text({
-				validation: { isRequired: true },
-				isIndexed: "unique",
-				isFilterable: true,
-			}),
-			password: password({ validation: { isRequired: true } }),
-			posts: relationship({ ref: "Post.author", many: true }),
-		},
-		ui: {
-			listView: {
-				initialColumns: ["name", "posts"],
-			},
-		},
-	}),
-	// Landing Page Form
-	LandingPage: list({
-		fields: {
-			title: text(),
-			header: text(),
-			visi: text(),
-			misi: multiselect({
-				options: [
-					{ label: "Misi 1", value: "misi1" },
-					{ label: "Misi 2", value: "misi2" },
-				],
-			}),
-			avatar: image({ storage: "my_local_images" }),
-			someFile: file({ storage: "my_s3_files" }),
-			about: text(),
-			portofolio: document({
-				formatting: true,
-			}),
-		},
-	}),
-	// Product Firm
-	Product: list({
-		fields: {
-			title: text(),
-		},
-	}),
-	// Carrer Form Input
-	Carrer: list({
-		fields: {
-			title: text(),
-			about: document({
-				formatting: true,
-			}),
-		},
-	}),
-	// Post Form
-	Post: list({
-		fields: {
-			title: text(),
-			status: select({
-				options: [
-					{ label: "Published", value: "published" },
-					{ label: "Draft", value: "draft" },
-				],
-				defaultValue: "draft",
-				ui: {
-					displayMode: "segmented-control",
-				},
-			}),
-			content: document({
-				formatting: true,
-				layouts: [
-					[1, 1],
-					[1, 1, 1],
-					[2, 1],
-					[1, 2],
-					[1, 2, 1],
-				],
-				links: true,
-				dividers: true,
-			}),
-			publishDate: timestamp(),
-			author: relationship({
-				ref: "User.posts",
-				ui: {
-					displayMode: "cards",
-					cardFields: ["name", "email"],
-					inlineEdit: { fields: ["name", "email"] },
-					linkToItem: true,
-					inlineConnect: true,
-				},
-			}),
-			tags: relationship({
-				ref: "Tag.posts",
-				ui: {
-					displayMode: "cards",
-					cardFields: ["name"],
-					inlineEdit: { fields: ["name"] },
-					linkToItem: true,
-					inlineConnect: true,
-					inlineCreate: { fields: ["name"] },
-				},
-				many: true,
-			}),
-		},
-	}),
+  User: list({
+    fields: {
+      name: text({ validation: { isRequired: true } }),
+      email: text({
+        validation: { isRequired: true },
+        isIndexed: "unique",
+        isFilterable: true,
+      }),
+      password: password({ validation: { isRequired: true } }),
+      posts: relationship({ ref: "Post.author", many: true }),
+    },
+    ui: {
+      listView: {
+        initialColumns: ["name", "posts"],
+      },
+    },
+  }),
+  // Landing Page Form
+  LandingPage: list({
+    fields: {
+      title: text(),
+      header: text(),
+      visi: text(),
+      misi: multiselect({
+        options: [
+          { label: "Misi 1", value: "misi1" },
+          { label: "Misi 2", value: "misi2" },
+        ],
+      }),
+      avatar: image({ storage: "my_local_images" }),
+      someFile: file({ storage: "my_s3_files" }),
+      about: text(),
+      portofolio: document({
+        formatting: true,
+      }),
+    },
+  }),
+  // Post Form
+  Post: list({
+    fields: {
+      title: text(),
+      status: select({
+        options: [
+          { label: "Published", value: "published" },
+          { label: "Draft", value: "draft" },
+        ],
+        defaultValue: "draft",
+        ui: {
+          displayMode: "segmented-control",
+        },
+      }),
+      content: document({
+        formatting: true,
+        layouts: [
+          [1, 1],
+          [1, 1, 1],
+          [2, 1],
+          [1, 2],
+          [1, 2, 1],
+        ],
+        links: true,
+        dividers: true,
+      }),
+      publishDate: timestamp(),
+      author: relationship({
+        ref: "User.posts",
+        ui: {
+          displayMode: "cards",
+          cardFields: ["name", "email"],
+          inlineEdit: { fields: ["name", "email"] },
+          linkToItem: true,
+          inlineConnect: true,
+        },
+      }),
+      tags: relationship({
+        ref: "Tag.posts",
+        ui: {
+          displayMode: "cards",
+          cardFields: ["name"],
+          inlineEdit: { fields: ["name"] },
+          linkToItem: true,
+          inlineConnect: true,
+          inlineCreate: { fields: ["name"] },
+        },
+        many: true,
+      }),
+    },
+  }),
 
-	// Tag Form
-	Tag: list({
-		ui: {
-			isHidden: true,
-		},
-		fields: {
-			name: text(),
-			posts: relationship({ ref: "Post.tags", many: true }),
-		},
-	}),
+  /* Carrer Schema */
+  Career: list(CareerSchema),
+
+  /** product schema */
+  Product: list(ProductSchema),
+
+  // Tag Form
+  Tag: list({
+    ui: {
+      isHidden: true,
+    },
+    fields: {
+      name: text(),
+      posts: relationship({ ref: "Post.tags", many: true }),
+    },
+  }),
 };
