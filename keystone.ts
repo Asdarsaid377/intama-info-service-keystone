8e1cc03fd5de9c9bbffd83e05b93b53003057939
Asdarsaid377 <asdarsaid377@gmail.com>
2022-09-13 15:10:07 +0700

    keystone

---
+++ keystone.ts
@@ -0,0 +1,47 @@
+import { config } from "@keystone-6/core";
+import { lists } from "./schema";
+import { withAuth, session } from "./auth";
+import dotenv from "dotenv";
+
+dotenv.config();
+const {
+	S3_BUCKET_NAME: bucketName = "keystone-test",
+	S3_REGION: region = "ap-southeast-2",
+	S3_ACCESS_KEY_ID: accessKeyId = "keystone",
+	S3_SECRET_ACCESS_KEY: secretAccessKey = "keystone",
+	ASSET_BASE_URL: baseUrl = "http://localhost:3000",
+} = process.env;
+
+export default withAuth(
+	config({
+		db: {
+			provider: "sqlite",
+			url: "file:./keystone.db",
+		},
+		ui: {
+			isAccessAllowed: (context) => !!context.session?.data,
+		},
+		session,
+		lists,
+		storage: {
+			my_local_images: {
+				kind: "local",
+				type: "image",
+				generateUrl: (path) => `${baseUrl}/images${path}`,
+				serverRoute: {
+					path: "/images",
+				},
+				storagePath: "public/images",
+			},
+			my_s3_files: {
+				kind: "s3",
+				type: "file",
+				bucketName,
+				region,
+				accessKeyId,
+				secretAccessKey,
+				signed: { expiry: 5000 },
+			},
+		},
+	})
+);
