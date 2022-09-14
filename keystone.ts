74f3ce80971ebc99097e3be556814caeed90e7e4
Asdarsaid377 <asdarsaid377@gmail.com>
2022-09-14 19:22:41 +0700

    update field apply

--- keystone.ts
+++ keystone.ts
@@ -5,54 +5,54 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const {
-  S3_BUCKET_NAME: bucketName = "keystone-test",
-  S3_REGION: region = "ap-southeast-2",
-  S3_ACCESS_KEY_ID: accessKeyId = "keystone",
-  S3_SECRET_ACCESS_KEY: secretAccessKey = "keystone",
-  ASSET_BASE_URL: baseUrl = "http://localhost:3000",
-  KEYSTONE_DB_USER,
-  KEYSTONE_DB_PASSWORD,
-  MYSQL_HOST_APP,
-  KEYSTONE_DB_NAME,
+	S3_BUCKET_NAME: bucketName = "keystone-test",
+	S3_REGION: region = "ap-southeast-2",
+	S3_ACCESS_KEY_ID: accessKeyId = "keystone",
+	S3_SECRET_ACCESS_KEY: secretAccessKey = "keystone",
+	ASSET_BASE_URL: baseUrl = "http://localhost:3000",
+	KEYSTONE_DB_USER,
+	KEYSTONE_DB_PASSWORD,
+	MYSQL_HOST_APP,
+	KEYSTONE_DB_NAME,
 } = process.env;
 
 export default withAuth(
-  config({
-    db: {
-      provider: "mysql",
-      url: `mysql://${KEYSTONE_DB_USER}:${KEYSTONE_DB_PASSWORD}@${MYSQL_HOST_APP}:3306/${KEYSTONE_DB_NAME}`,
-      onConnect: async (context) => {
-        /* ... */
-      },
-      // Optional advanced configuration
-      enableLogging: true,
-      useMigrations: true,
-      idField: { kind: "uuid" },
-    },
-    ui: {
-      isAccessAllowed: (context) => !!context.session?.data,
-    },
-    session,
-    lists,
-    storage: {
-      my_local_images: {
-        kind: "local",
-        type: "image",
-        generateUrl: (path) => `${baseUrl}/images${path}`,
-        serverRoute: {
-          path: "/images",
-        },
-        storagePath: "public/images",
-      },
-      my_s3_files: {
-        kind: "s3",
-        type: "file",
-        bucketName,
-        region,
-        accessKeyId,
-        secretAccessKey,
-        signed: { expiry: 5000 },
-      },
-    },
-  })
+	config({
+		db: {
+			provider: "mysql",
+			url: `mysql://${KEYSTONE_DB_USER}:${KEYSTONE_DB_PASSWORD}@${MYSQL_HOST_APP}:3306/${KEYSTONE_DB_NAME}`,
+			onConnect: async (context) => {
+				/* ... */
+			},
+			// Optional advanced configuration
+			enableLogging: true,
+			useMigrations: true,
+			idField: { kind: "uuid" },
+		},
+		ui: {
+			isAccessAllowed: (context) => !!context.session?.data,
+		},
+		session,
+		lists,
+		storage: {
+			my_local_images: {
+				kind: "local",
+				type: "image",
+				generateUrl: (path) => `${baseUrl}/images${path}`,
+				serverRoute: {
+					path: "/images",
+				},
+				storagePath: "public/images",
+			},
+			my_s3_files: {
+				kind: "s3",
+				type: "file",
+				bucketName,
+				region,
+				accessKeyId,
+				secretAccessKey,
+				signed: { expiry: 5000 },
+			},
+		},
+	})
 );
